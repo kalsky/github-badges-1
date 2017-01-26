016f1a5d44ebffb27434b3d9950ae572a52e1ffe
Yaniv Kalsky <yanivka@gmail.com>
2017-01-26 09:49:50 -0800

    Looking for issues

--- app.rb
+++ app.rb
@@ -27,16 +27,18 @@ end
         style    = params[:style] ? params[:style] : 'default'
         bg_color = params[:background] ? params[:background] : '4c1' # default bg color to green
         color    = params[:color] ? params[:color] : 'fff' # default text color to white
+        issue    = params[:issue]
+        auth     = params[:auth]
 
         case type
-          when :star
-            count = fetch("https://api.github.com/repos/#{user}/#{repo}", 'stargazers_count', {user: user, repo: repo})
-            count_url = "https://github.com/#{user}/#{repo}/stargazers"
-            button_url = "https://github.com/#{user}/#{repo}"
-          when :fork
-            count = fetch("https://api.github.com/repos/#{user}/#{repo}", 'forks_count', {user: user, repo: repo})
-            count_url = "https://github.com/#{user}/#{repo}/network"
-            button_url = "https://github.com/#{user}/#{repo}/fork"
+          when :state
+            count = fetch("https://api.github.com/repos/#{user}/#{repo}/issues/#{issue}", 'state', {user: user, repo: repo})
+            count_url = "https://github.com/#{user}/#{repo}/issues/#{issue}"
+            button_url = "https://github.com/#{user}/#{repo}/issues/#{issue}"
+          when :milestone
+            count = fetch("https://api.github.com/repos/#{user}/#{repo}/issues/#{issue}", 'milestone.state', {user: user, repo: repo})
+            count_url = "https://github.com/#{user}/#{repo}/issues/#{issue}"
+            button_url = "https://github.com/#{user}/#{repo}/issues/#{issue}"
         end
 
         # everything is ok.
@@ -84,7 +86,11 @@ def fetch(api_url, prop, args)
     io.write response.body
   end if response.is_a? Net::HTTPSuccess
 
-  JSON.parse(File.read(cached_response))[prop]
+  proplist = prop.split('.')
+  if (proplist.length==1)
+    JSON.parse(File.read(cached_response))[prop]
+  else if (proplist.length==2)
+    JSON.parse(File.read(cached_response))[proplist[0]][proplist[1]]
 end
 
 def create_button(opts)
